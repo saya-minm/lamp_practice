05fad34c7358c547828ab67a9343148f9064bf67
Saya Shishido <minm37412@gmail.com>
2019-11-22 08:39:35 +0900

    #17 並び替えのプルダウンを作成しました

--- www/html/sort.php
+++ www/html/sort.php
@@ -1,8 +1,10 @@
 <?php
 require_once '../conf/const.php';
-require_once '../model/functions.php';
-require_once '../model/user.php';
-require_once '../model/item.php';
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'user.php';
+require_once MODEL_PATH . 'item.php';
+require_once MODEL_PATH . 'cart.php';
+require_once MODEL_PATH . 'sort.php';
 
 session_start();
 
@@ -12,10 +14,11 @@ if(is_logined() === false){
 
 $db = get_db_connect();
 $user = get_login_user($db);
-
-$items = get_open_items($db);
-
+$sort = get_post("sort");
 $new = array('新着順','価格の安い順','価格の高い順');
+$keys = (int)$sort;
+
+$items = sort_new($db, $keys);
 
 
 include_once '../view/index_view.php';
